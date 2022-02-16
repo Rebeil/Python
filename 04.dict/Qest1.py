e2c30cea9f9d5178b9cd4a8d2396605ca7634ba2
Rebeil <dany160301@yandex.ru>
2022-02-16 23:00:36 +0400

    The first task for the topic is a dict

---
+++ 04.dict/Qest1.py
@@ -0,0 +1,9 @@
+ids = {'user1': [213, 213, 213, 15, 213], 
+       'user2': [54, 54, 119, 119, 119], 
+       'user3': [213, 98, 98, 35]}
+a = set()
+
+for v in ids.values():
+    for i in v:
+        a.add(i)
+print(a)
