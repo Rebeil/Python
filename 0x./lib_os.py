0da59d9f9c346f2159e659eb7a525af0a7d79418
Rebeil <dany160301@yandex.ru>
2022-02-19 22:47:34 +0400

    update lib_os

--- 0x./lib_os.py
+++ 0x./lib_os.py
@@ -6,4 +6,8 @@ print(f"Current workong directory is {os.getcwd()}")
 
 print(os.path.exists('Python'))
 
+#смотрит из текущей директории
 print(os.path.exists(os.path.join('0x.', 'lib_os.py')))
+
+#подкаталоги test_dir->level_1->level_2->level_3
+os.makedirs(os.path.join('test_dir', 'level_1', 'level_2', 'level_3'))
