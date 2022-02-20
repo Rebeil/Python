8e2edd612533f8058f4bda4c70a9a450692db366
Rebeil <dany160301@yandex.ru>
2022-02-20 21:41:43 +0400

    rename 0x. in x.

---
+++ x./lib_os.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import os
+
+print(f"Current workong directory is {os.getcwd()}")
+
+print(os.path.exists('Python'))
+
+#смотрит из текущей директории
+print(os.path.exists(os.path.join('0x.', 'lib_os.py')))
+
+#подкаталоги test_dir->level_1->level_2->level_3
+#os.makedirs(os.path.join('test_dir', 'level_1', 'level_2', 'level_3'))
+
+print(os.listdir('/home/rain//Python'))
+
